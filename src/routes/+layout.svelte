bb1924e2a6771918f64a95784449df69788e9261
Davi Neves <davi.neves.santos2@gmail.com>
2024-11-21 10:30:35 +0000

    stuff

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+    // @ts-nocheck
+
+    import MainNavbar from "$lib/components/MainNavbar.svelte";
+</script>
+
+<div class="flex flex-col h-screen">
+    <MainNavbar />
+
+    <main class="flex-1 p-4 mt-16">
+        <slot />
+    </main>
+
+    <footer class="bg-gray-200 text-center text-sm p-4 mt-8">
+        © 2024 My blog
+    </footer>
+</div>
